f1d2f2158ee4d71f1c9372a3582712d15a4ba42b
Michael <michael.a.colozzi@gmail.com>
2026-02-04 21:45:34 -0700

    handlization of the app

--- main.m
+++ main.m
@@ -25,7 +25,7 @@ end
 clear all
 close all hidden
 rng(1234)
-N = 3000;
+N = 100;
 trueStates = zeros(4,1,N);
 trackStates = zeros(4,N);
 
@@ -38,8 +38,8 @@ InitialCovPad(3:4,3:4) = 9 * eye(2);
 Q = zeros(4);
 Q(1:2,1:2) = 0.00001 * eye(2);
 Q(3:4,3:4) = 0.01 * eye(2);
-Trker = JPDA      (0.9,... %PD
-                  1e-6,... %Beta 
+Trker = GNN      (0.9,... %PD
+                  1e-6,... %Beta %3,... % number of Lags
                   A,Q,[eye(2),zeros(2)],InitialCovPad);
 
 NumTargets = 5;
